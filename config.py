ef57fd051d69bd58afbd84be82927726cdd53315
SaadSafa <saadsafadieh2@gmail.com>
2025-08-29 11:43:22 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,28 @@
+
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['safadiehsaad3@gmail.com']
+
+    POSTS_PER_PAGE = 10
+
+    LANGUAGES = ['en','ar','es','fn']
+
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    MS_TRANSLATOR_REGION = os.environ.get('MS_TRANSLATOR_REGION')
+
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTIC_PASSWORD = os.environ.get('ELASTIC_PASSWORD')
