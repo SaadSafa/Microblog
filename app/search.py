5086f0a499e981c0cf4ff6015c54b30b53c06462
SaadSafa <saadsafadieh2@gmail.com>
2025-08-30 14:46:57 +0300

    Fix Elasticsearch

--- app/search.py
+++ app/search.py
@@ -17,10 +17,18 @@ def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
-        index = index,
-        query = {'multi_match': {'query': query, 'fields': ['*']}},
-        from_ = (page - 1) * per_page,
-        size = per_page
+        index=index,
+        body={
+            "query": {
+                "multi_match": {
+                    "query": query,
+                    "fields": ["*"]
+                }
+            },
+            "from": (page - 1) * per_page,
+            "size": per_page
+        }
     )
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
-    return ids, search['hits']['total']['value']
+    total = search['hits']['total']['value']
+    return ids, total
